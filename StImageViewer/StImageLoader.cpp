5ce26a1b8e11a250b27205d02fc0bd8e85b84a88
Kirill Gavrilov <kirill@sview.ru>
2014-01-18 14:45:56 +0400

    StImageLoader - cosmetics

--- StImageViewer/StImageLoader.cpp
+++ StImageViewer/StImageLoader.cpp
@@ -1,5 +1,5 @@
 /**
- * Copyright © 2007-2013 Kirill Gavrilov <kirill@sview.ru>
+ * Copyright © 2007-2014 Kirill Gavrilov <kirill@sview.ru>
  *
  * StImageViewer program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -26,10 +26,22 @@
 
 const char* StImageLoader::ST_IMAGES_MIME_STRING = ST_IMAGE_PLUGIN_MIME_CHAR;
 
-static SV_THREAD_FUNCTION threadFunction(void* inStImageLoader) {
-    StImageLoader* stImageLoader = (StImageLoader* )inStImageLoader;
-    stImageLoader->mainLoop();
-    return SV_THREAD_RETURN 0;
+namespace {
+
+    static StString formatError(const StString& theFilePath,
+                                const StString& theImgLibDescr) {
+        StString aFileName, aFolderName;
+        StFileNode::getFolderAndFile(theFilePath, aFolderName, aFileName);
+        ST_ERROR_LOG("Can not load image file \"" + theFilePath + "\" (" + theImgLibDescr + ')');
+        return StString("Can not load image file:\n\"") + aFileName + "\"\n" + theImgLibDescr;
+    }
+
+    static SV_THREAD_FUNCTION threadFunction(void* theImageLoader) {
+        StImageLoader* anImageLoader = (StImageLoader* )theImageLoader;
+        anImageLoader->mainLoop();
+        return SV_THREAD_RETURN 0;
+    }
+
 }
 
 StImageLoader::StImageLoader(const StImageFile::ImageClass     theImageLib,
@@ -62,13 +74,6 @@ void StImageLoader::setCompressMemory(const bool theToCompress) {
     myTextureQueue->setCompressMemory(theToCompress);
 }
 
-static StString formatError(const StString& theFilePath, const StString& theImgLibDescr) {
-    StString aFileName, aFolderName;
-    StFileNode::getFolderAndFile(theFilePath, aFolderName, aFileName);
-    ST_ERROR_LOG("Can not load image file \"" + theFilePath + "\" (" + theImgLibDescr + ')');
-    return StString("Can not load image file:\n\"") + aFileName + "\"\n" + theImgLibDescr;
-}
-
 void StImageLoader::processLoadFail(const StString& theErrorDesc) {
     myMsgQueue->pushError(theErrorDesc);
     myTextureQueue->setConnectedStream(false);
@@ -77,12 +82,13 @@ void StImageLoader::processLoadFail(const StString& theErrorDesc) {
 
 bool StImageLoader::loadImage(const StHandle<StFileNode>& theSource,
                               StHandle<StStereoParams>&   theParams) {
-    StString fileToLoadPath = theSource->getPath();
-    StImageFile::ImageType anImgType = StImageFile::guessImageType(fileToLoadPath, theSource->getMIME());
+    const StString               aFilePath = theSource->getPath();
+    const StImageFile::ImageType anImgType = StImageFile::guessImageType(aFilePath, theSource->getMIME());
 
-    StHandle<StImageFile> stImageL = StImageFile::create(myImageLib, anImgType);
-    StHandle<StImageFile> stImageR = StImageFile::create(myImageLib, anImgType);
-    if(stImageL.isNull() || stImageR.isNull()) {
+    StHandle<StImageFile> anImageL = StImageFile::create(myImageLib, anImgType);
+    StHandle<StImageFile> anImageR = StImageFile::create(myImageLib, anImgType);
+    if(anImageL.isNull()
+    || anImageR.isNull()) {
         processLoadFail("No any image library was found!");
         return false;
     }
@@ -94,74 +100,75 @@ bool StImageLoader::loadImage(const StHandle<StFileNode>& theSource,
         // special procedure to divide MPO (Multi Picture Object)
         StJpegParser aJpegParser;
         double anHParallax = 0.0; // parallax in percents
-        if(!aJpegParser.read(fileToLoadPath)) {
-            processLoadFail(StString("Can not read the file \"") + fileToLoadPath + '\"');
+        if(!aJpegParser.read(aFilePath)) {
+            processLoadFail(StString("Can not read the file \"") + aFilePath + '\"');
             return false;
         }
 
         // read image from memory
         StHandle<StJpegParser::Image> anImg1 = aJpegParser.getImage(0);
 
-        StJpegParser::Orient anOrient = anImg1->getOrientation();
+        const StJpegParser::Orient anOrient = anImg1->getOrientation();
         theParams->setZRotateZero((GLfloat )StJpegParser::getRotationAngle(anOrient));
         anImg1->getParallax(anHParallax);
-        if(!stImageL->load(fileToLoadPath, StImageFile::ST_TYPE_JPEG,
+        if(!anImageL->load(aFilePath, StImageFile::ST_TYPE_JPEG,
                            (uint8_t* )anImg1->myData, (int )anImg1->myLength)
-        && !stImageL->load(fileToLoadPath, StImageFile::ST_TYPE_JPEG,
+        && !anImageL->load(aFilePath, StImageFile::ST_TYPE_JPEG,
                            (uint8_t* )aJpegParser.getData(), (int )aJpegParser.getDataSize())) {
-            processLoadFail(formatError(fileToLoadPath, stImageL->getState()));
+            processLoadFail(formatError(aFilePath, anImageL->getState()));
             return false;
         }
 
         StHandle<StJpegParser::Image> anImg2 = aJpegParser.getImage(1);
-        if(!anImg2.isNull() && anImgType == StImageFile::ST_TYPE_MPO) {
+        if(!anImg2.isNull()
+         && anImgType == StImageFile::ST_TYPE_MPO) {
             // read image from memory
             anImg2->getParallax(anHParallax); // in MPO parallax generally stored ONLY in second frame
-            if(!stImageR->load(fileToLoadPath, StImageFile::ST_TYPE_JPEG,
+            if(!anImageR->load(aFilePath, StImageFile::ST_TYPE_JPEG,
                                (uint8_t* )anImg2->myData, (int )anImg2->myLength)) {
-                processLoadFail(formatError(fileToLoadPath, stImageR->getState()));
-                stImageL->close();
-                stImageL->nullify();
+                processLoadFail(formatError(aFilePath, anImageR->getState()));
+                anImageL->close();
+                anImageL->nullify();
                 return false;
             }
 
             // convert percents to pixels
-            theParams->setSeparationNeutral(GLint(anHParallax * stImageR->getSizeX() * 0.01));
+            theParams->setSeparationNeutral(GLint(anHParallax * anImageR->getSizeX() * 0.01));
         } else if(anImgType == StImageFile::ST_TYPE_MPO) {
-            ST_DEBUG_LOG("MPO image \"" + fileToLoadPath + "\" is invalid!");
+            ST_DEBUG_LOG("MPO image \"" + aFilePath + "\" is invalid!");
         }
     } else if(theSource->size() >= 2) {
-        StString fileToLoadPathLeft  = theSource->getValue(0)->getPath();
-        StString fileToLoadPathRight = theSource->getValue(1)->getPath();
+        const StString aFilePathLeft  = theSource->getValue(0)->getPath();
+        const StString aFilePathRight = theSource->getValue(1)->getPath();
 
         // loading image with format autodetection
-        if(!stImageL->load(fileToLoadPathLeft)) {
-            processLoadFail(formatError(fileToLoadPathLeft, stImageL->getState()));
+        if(!anImageL->load(aFilePathLeft)) {
+            processLoadFail(formatError(aFilePathLeft, anImageL->getState()));
             return false;
         }
-        if(!stImageR->load(fileToLoadPathRight)) {
-            processLoadFail(formatError(fileToLoadPathRight, stImageR->getState()));
-            stImageL->close();
-            stImageL->nullify();
+        if(!anImageR->load(aFilePathRight)) {
+            processLoadFail(formatError(aFilePathRight, anImageR->getState()));
+            anImageL->close();
+            anImageL->nullify();
             return false;
         }
     } else {
         if(mySrcFormat == ST_V_SRC_AUTODETECT && (anImgType == StImageFile::ST_TYPE_JPS || anImgType == StImageFile::ST_TYPE_PNS)) {
             aSrcFormatCurr = ST_V_SRC_SIDE_BY_SIDE;
         }
-        if(!stImageL->load(fileToLoadPath, anImgType)) {
-            processLoadFail(formatError(fileToLoadPath, stImageL->getState()));
+        if(!anImageL->load(aFilePath, anImgType)) {
+            processLoadFail(formatError(aFilePath, anImageL->getState()));
             return false;
         }
     }
     const double aLoadTimeMSec = aLoadTimer.getElapsedTimeInMilliSec();
 
 #ifdef __ST_DEBUG__
-    if(!stImageL->isNull()) {
-        ST_DEBUG_LOG(stImageL->getState());
+    if(!anImageL->isNull()) {
+        ST_DEBUG_LOG(anImageL->getState());
     }
-    if(!stImageR->isNull()) {
-        ST_DEBUG_LOG(stImageR->getState());
+    if(!anImageR->isNull()) {
+        ST_DEBUG_LOG(anImageR->getState());
     }
 #endif
 
@@ -171,10 +178,10 @@ bool StImageLoader::loadImage(const StHandle<StFileNode>& theSource,
     }
 
     myTextureQueue->setConnectedStream(true);
-    if(!stImageR->isNull()) {
-        myTextureQueue->push(*stImageL, *stImageR, theParams, ST_V_SRC_SEPARATE_FRAMES, 0.0);
+    if(!anImageR->isNull()) {
+        myTextureQueue->push(*anImageL, *anImageR, theParams, ST_V_SRC_SEPARATE_FRAMES, 0.0);
     } else {
-        myTextureQueue->push(*stImageL, *stImageR, theParams, aSrcFormatCurr, 0.0);
+        myTextureQueue->push(*anImageL, *anImageR, theParams, aSrcFormatCurr, 0.0);
     }
 
     StHandle<StImageInfo> anImgInfo = new StImageInfo();
@@ -187,29 +194,29 @@ bool StImageLoader::loadImage(const StHandle<StFileNode>& theSource,
         StFileNode::getFolderAndFile(theSource->getValue(1)->getPath(), aFolder, aTitleString);
         anImgInfo->myInfo.add(StArgument("Name (R)", aTitleString));
     } else {
-        StFileNode::getFolderAndFile(fileToLoadPath, aFolder, aTitleString);
+        StFileNode::getFolderAndFile(aFilePath, aFolder, aTitleString);
         anImgInfo->myInfo.add(StArgument("Name", aTitleString));
     }
-    if(!stImageR->isNull()) {
-        anImgInfo->myInfo.add(StArgument("Dimensions (L)",  StString() + stImageL->getSizeX()
-                                                               + " x " + stImageL->getSizeY()));
-        anImgInfo->myInfo.add(StArgument("Dimensions (R)",  StString() + stImageR->getSizeX()
-                                                               + " x " + stImageR->getSizeY()));
-        anImgInfo->myInfo.add(StArgument("Color Model (L)", stImageL->formatImgColorModel()));
-        anImgInfo->myInfo.add(StArgument("Color Model (R)", stImageR->formatImgColorModel()));
+    if(!anImageR->isNull()) {
+        anImgInfo->myInfo.add(StArgument("Dimensions (L)",  StString() + anImageL->getSizeX()
+                                                               + " x " + anImageL->getSizeY()));
+        anImgInfo->myInfo.add(StArgument("Dimensions (R)",  StString() + anImageR->getSizeX()
+                                                               + " x " + anImageR->getSizeY()));
+        anImgInfo->myInfo.add(StArgument("Color Model (L)", anImageL->formatImgColorModel()));
+        anImgInfo->myInfo.add(StArgument("Color Model (R)", anImageR->formatImgColorModel()));
     } else {
-        anImgInfo->myInfo.add(StArgument("Dimensions",      StString() + stImageL->getSizeX()
-                                                               + " x " + stImageL->getSizeY()));
-        anImgInfo->myInfo.add(StArgument("Color Model",     stImageL->formatImgColorModel()));
+        anImgInfo->myInfo.add(StArgument("Dimensions",      StString() + anImageL->getSizeX()
+                                                               + " x " + anImageL->getSizeY()));
+        anImgInfo->myInfo.add(StArgument("Color Model",     anImageL->formatImgColorModel()));
     }
     anImgInfo->myInfo.add(StArgument("Load time", StString(aLoadTimeMSec) + " msec"));
     myImgInfo = anImgInfo;
 
     // clean up - close opened files and reset memory
-    stImageL->close();
-    stImageL->nullify();
-    stImageR->close();
-    stImageR->nullify();
+    anImageL->close();
+    anImageL->nullify();
+    anImageR->close();
+    anImageR->nullify();
 
     myTextureQueue->stglSwapFB(0);
 
@@ -221,52 +228,54 @@ bool StImageLoader::loadImage(const StHandle<StFileNode>& theSource,
 bool StImageLoader::saveImage(const StHandle<StFileNode>&     theSource,
                               const StHandle<StStereoParams>& theParams,
                               StImageFile::ImageType          theImgType) {
-    if(theParams.isNull() || theParams.isNull() || theImgType == StImageFile::ST_TYPE_NONE) {
+    if(theParams.isNull()
+    || theImgType == StImageFile::ST_TYPE_NONE) {
         stInfo(myLangMap->getValue(StImageViewerStrings::DIALOG_NOTHING_TO_SAVE));
         return false;
     }
 
-    int result = StGLTextureQueue::SNAPSHOT_NO_NEW;
-    StImage dataLeft;
-    StImage dataRight;
+    int aResult = StGLTextureQueue::SNAPSHOT_NO_NEW;
+    StImage aDataLeft, aDataRight;
     if(!theParams->isSwapLR()) {
-        result = getSnapshot(&dataLeft, &dataRight, true);
+        aResult = getSnapshot(&aDataLeft, &aDataRight, true);
     } else {
-        result = getSnapshot(&dataRight, &dataLeft, true);
+        aResult = getSnapshot(&aDataRight, &aDataLeft, true);
     }
 
-    if(result == StGLTextureQueue::SNAPSHOT_NO_NEW || dataLeft.isNull()) {
+    if(aResult == StGLTextureQueue::SNAPSHOT_NO_NEW
+    || aDataLeft.isNull()) {
         stInfo(myLangMap->getValue(StImageViewerStrings::DIALOG_NO_SNAPSHOT));
         return false;
     }
-    StHandle<StImageFile> dataResult = StImageFile::create(myImageLib);
-    if(dataResult.isNull()) {
+    StHandle<StImageFile> aDataResult = StImageFile::create(myImageLib);
+    if(aDataResult.isNull()) {
         myMsgQueue->pushError(stCString("No any image library was found!"));
         return false;
     }
 
-    bool toSaveStereo = !dataRight.isNull();
-    if(toSaveStereo && dataResult->initSideBySide(dataLeft, dataRight,
-                                                  theParams->getSeparationDx(),
-                                                  theParams->getSeparationDy())) {
-        dataLeft.nullify();
-        dataRight.nullify();
+    const bool toSaveStereo = !aDataRight.isNull();
+    if(toSaveStereo
+    && aDataResult->initSideBySide(aDataLeft, aDataRight,
+                                   theParams->getSeparationDx(),
+                                   theParams->getSeparationDy())) {
+        aDataLeft.nullify();
+        aDataRight.nullify();
     } else {
-        dataResult->initWrapper(dataLeft);
+        aDataResult->initWrapper(aDataLeft);
     }
 
     const StString& aTitle = myLangMap->getValue(StImageViewerStrings::DIALOG_SAVE_SNAPSHOT);
-    StMIMEList filter;
-    StString saveExt;
+    StMIMEList aFilter;
+    StString aSaveExt;
     if(toSaveStereo) {
         switch(theImgType) {
             case StImageFile::ST_TYPE_PNG:
-                saveExt = ST_PNS_EXT;
-                filter.add(StMIME(ST_PNS_MIME, saveExt, ST_PNS_DESC));
+                aSaveExt = ST_PNS_EXT;
+                aFilter.add(StMIME(ST_PNS_MIME, aSaveExt, ST_PNS_DESC));
                 break;
             case StImageFile::ST_TYPE_JPEG:
-                saveExt = ST_JPS_EXT;
-                filter.add(StMIME(ST_JPS_MIME, saveExt, ST_JPS_DESC));
+                aSaveExt = ST_JPS_EXT;
+                aFilter.add(StMIME(ST_JPS_MIME, aSaveExt, ST_JPS_DESC));
                 break;
             default:
                 return false;
@@ -274,28 +283,28 @@ bool StImageLoader::saveImage(const StHandle<StFileNode>&     theSource,
     } else {
         switch(theImgType) {
             case StImageFile::ST_TYPE_PNG:
-                saveExt = ST_PNG_EXT;
-                filter.add(StMIME(ST_PNG_MIME, saveExt, ST_PNG_DESC));
+                aSaveExt = ST_PNG_EXT;
+                aFilter.add(StMIME(ST_PNG_MIME, aSaveExt, ST_PNG_DESC));
                 break;
             case StImageFile::ST_TYPE_JPEG:
-                saveExt = ST_JPG_EXT;
-                filter.add(StMIME(ST_JPG_MIME, saveExt, ST_JPEG_DESC));
+                aSaveExt = ST_JPG_EXT;
+                aFilter.add(StMIME(ST_JPG_MIME, aSaveExt, ST_JPEG_DESC));
                 break;
             default:
                 return false;
         }
     }
 
-    StString fileToSave;
-    if(StFileNode::openFileDialog(theSource->getFolderPath(), aTitle, filter, fileToSave, true)) {
-        if(StFileNode::getExtension(fileToSave) != saveExt) {
-            fileToSave += StString('.') + saveExt;
+    StString aFileToSave;
+    if(StFileNode::openFileDialog(theSource->getFolderPath(), aTitle, aFilter, aFileToSave, true)) {
+        if(StFileNode::getExtension(aFileToSave) != aSaveExt) {
+            aFileToSave += StString('.') + aSaveExt;
         }
 
-        bool toSave = !StFileNode::isFileExists(fileToSave);
+        bool toSave = !StFileNode::isFileExists(aFileToSave);
         if(!toSave) {
             if(stQuestion("File already exists!\nOverride the file?")) {
-                toSave = StFileNode::removeFile(fileToSave);
+                toSave = StFileNode::removeFile(aFileToSave);
                 if(!toSave) {
                     myMsgQueue->pushError(stCString("Could not remove the file!"));
                     return false;
@@ -304,15 +313,15 @@ bool StImageLoader::saveImage(const StHandle<StFileNode>&     theSource,
         }
 
         if(toSave) {
-            ST_DEBUG_LOG("Save snapshot to the path '" + fileToSave + '\'');
+            ST_DEBUG_LOG("Save snapshot to the path '" + aFileToSave + '\'');
             StString strSaveState;
-            if(!dataResult->save(fileToSave, theImgType)) {
+            if(!aDataResult->save(aFileToSave, theImgType)) {
                 // TODO (Kirill Gavrilov#7)
-                myMsgQueue->pushError(dataResult->getState());
+                myMsgQueue->pushError(aDataResult->getState());
                 return false;
             }
-            if(!dataResult->getState().isEmpty()) {
-                ST_DEBUG_LOG(dataResult->getState());
+            if(!aDataResult->getState().isEmpty()) {
+                ST_DEBUG_LOG(aDataResult->getState());
             }
             // TODO (Kirill Gavrilov#8) - update playlist (append new file)
         }
@@ -321,7 +330,7 @@ bool StImageLoader::saveImage(const StHandle<StFileNode>&     theSource,
 }
 
 void StImageLoader::mainLoop() {
-    StHandle<StFileNode> aFileToLoad;
+    StHandle<StFileNode>     aFileToLoad;
     StHandle<StStereoParams> aFileParams;
     for(;;) {
         myLoadNextEvent.wait();
