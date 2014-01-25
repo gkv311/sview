331b1d47ad1bf14fabfcf6277043ad7aba52d336
Kirill Gavrilov <kirill@sview.ru>
2014-01-25 11:33:16 +0400

    copy_res.sh - copy Korean translation

--- copy_res.sh
+++ copy_res.sh
@@ -8,10 +8,12 @@ mkdir -p "${TARGET_OUTPUT_DIR}lang/English/"
 mkdir -p "${TARGET_OUTPUT_DIR}lang/русский/"
 mkdir -p "${TARGET_OUTPUT_DIR}lang/français/"
 mkdir -p "${TARGET_OUTPUT_DIR}lang/Deutsch/"
+mkdir -p "${TARGET_OUTPUT_DIR}lang/Korean/"
 cp -f -r lang/english/* "${TARGET_OUTPUT_DIR}lang/English/"
 cp -f -r lang/russian/* "${TARGET_OUTPUT_DIR}lang/русский/"
 cp -f -r lang/french/*  "${TARGET_OUTPUT_DIR}lang/français/"
 cp -f -r lang/german/*  "${TARGET_OUTPUT_DIR}lang/Deutsch/"
+cp -f -r lang/korean/*  "${TARGET_OUTPUT_DIR}lang/Korean/"
 
 if [ -d "shaders" ]; then
   mkdir -p "${TARGET_OUTPUT_DIR}shaders/${PROJECT_NAME}"
