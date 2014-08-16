cc60610cbbc245f035b2880b82538384681b88cc
Kirill Gavrilov <kirill@sview.ru>
2014-08-16 15:48:17 +0400

    StCheckUpdates - use StRawFile instead of StSocket

--- include/StSocket/StCheckUpdates.h
+++ include/StSocket/StCheckUpdates.h
@@ -1,5 +1,5 @@
 /**
- * Copyright © 2009-2010 Kirill Gavrilov <kirill@sview.ru>
+ * Copyright © 2009-2014 Kirill Gavrilov <kirill@sview.ru>
  *
  * Distributed under the Boost Software License, Version 1.0.
  * See accompanying file license-boost.txt or copy at
@@ -9,7 +9,8 @@
 #ifndef __StCheckUpdates_h_
 #define __StCheckUpdates_h_
 
-#include <StSocket/StSocket.h>
+#include <StFile/StRawFile.h>
+
 #include <StThreads/StMutex.h>
 #include <StThreads/StThread.h>
 #include <StVersion.h>
@@ -19,127 +20,93 @@
  */
 class StCheckUpdates {
 
-        private:
-
-    StMutex stMutex;
-    StThread* hCheckUpdates;
-    bool bInitialized;
-    bool bNeedUpdate;
-
-    static SV_THREAD_FUNCTION checkUpdatesThread(void* inCheckUpdates) {
-        StCheckUpdates* stCheckUpdates = (StCheckUpdates* )inCheckUpdates;
-        if(StCheckUpdates::checkUpdates()) {
-            stCheckUpdates->setNeedUpdate(true);
-        }
-        stCheckUpdates->setInitialized();
-        return SV_THREAD_RETURN 0;
-    }
-
         public:
 
     StCheckUpdates()
-    : stMutex(),
-      hCheckUpdates(NULL),
-      bInitialized(false),
-      bNeedUpdate(false) {
+    : myIsInitialized(false),
+      myToUpdate(false) {
         //
     }
 
     ~StCheckUpdates() {
-        if(hCheckUpdates != NULL) {
-            hCheckUpdates->wait();
-            delete hCheckUpdates;
-            hCheckUpdates = NULL;
+        if(!myThread.isNull()) {
+            myThread->wait();
         }
     }
 
     bool isNeedUpdate() {
-        stMutex.lock();
-            bool result = bNeedUpdate;
-        stMutex.unlock();
-        return result;
+        myMutex.lock();
+        const bool aResult = myToUpdate;
+        myMutex.unlock();
+        return aResult;
     }
 
-    void setNeedUpdate(bool toUpdate) {
-        stMutex.lock();
-            bNeedUpdate = toUpdate;
-        stMutex.unlock();
+    void setNeedUpdate(bool theToUpdate) {
+        myMutex.lock();
+        myToUpdate = theToUpdate;
+        myMutex.unlock();
     }
 
     bool isInitialized() {
-        stMutex.lock();
-            bool result = bInitialized;
-        stMutex.unlock();
-        return result;
+        myMutex.lock();
+        const bool aResult = myIsInitialized;
+        myMutex.unlock();
+        return aResult;
     }
 
     void setInitialized() {
-        stMutex.lock();
-            bInitialized = true;
-        stMutex.unlock();
+        myMutex.lock();
+        myIsInitialized = true;
+        myMutex.unlock();
     }
 
     void init() {
-        /// TODO (Kirill Gavrilov#1) unstability in browsers???
-        stMutex.lock();
-        if(hCheckUpdates != NULL) {
-            stMutex.unlock();
+        /// TODO (Kirill Gavrilov#1) instability in browsers???
+        myMutex.lock();
+        if(!myThread.isNull()) {
+            myMutex.unlock();
             return;
         }
-        hCheckUpdates = new StThread(checkUpdatesThread, (void* )this);
-        stMutex.unlock();
+        myThread = new StThread(checkUpdatesThread, this);
+        myMutex.unlock();
     }
 
     static bool checkUpdates() {
-        static const char SVIEW_UPDATES_SERVER[] = "www.sview.ru";
-        static const char sViewUpdateRequestTemplate[] = "GET /updates/?appl=sView&year=%d&month=%d&rStatus=%d&rSubVer=%d HTTP/1.1\r\nUser-Agent: sView\r\nHost: www.sview.ru\r\n\r\n";
-        static const StVersionInfo stVersion = StVersionInfo::getSDKVersionInfo();
-        char sViewUpdateRequest[2048];
-        sprintf(sViewUpdateRequest, sViewUpdateRequestTemplate, stVersion.getYear(), stVersion.getMonth(), stVersion.getReleaseStatus(), stVersion.getSubVersion());
-        ///sprintf(sViewUpdateRequest, sViewUpdateRequestTemplate, stVersion.getYear(), 8, stVersion.getReleaseStatus(), stVersion.getSubVersion());
+        const char     THE_URL_TEMPLATE[] = "http://www.sview.ru/updates/?appl=sView&year=%d&month=%d&rStatus=%d&rSubVer=%d";
+        const StString THE_TO_UPDATE      = "<update>yes</update>";
 
-        StSocket stSocket;
-        if(!stSocket.open()) {
+        const StVersionInfo aCurrVer = StVersionInfo::getSDKVersionInfo();
+        char anUrl[2048];
+        sprintf(anUrl, THE_URL_TEMPLATE, aCurrVer.getYear(), aCurrVer.getMonth(), aCurrVer.getReleaseStatus(), aCurrVer.getSubVersion());
+
+        const StString anAnswer = StRawFile::readTextFile(StString(anUrl));
+        if(anAnswer.isEmpty()) {
             return false;
         }
-        ST_DEBUG_LOG_AT("Opened socket...");
-
-        if(!stSocket.connect(SVIEW_UPDATES_SERVER)) {
-            ST_DEBUG_LOG_AT("ERROR connecting...");
-            return false;
-        }
-        ST_DEBUG_LOG_AT("Connection opened...");
-
-        StSocket::Buffer buffer(2048);
-        buffer = sViewUpdateRequest;
-
-        // TODO (Kirill Gavrilov#9#) ccreate HTTP-helper class
-        if(!stSocket.send(buffer)) {
-            ST_DEBUG_LOG_AT("ERROR sending...");
-            return false;
-        }
-        ST_DEBUG_LOG("Sent:\n" + buffer.getData());
-
-        // TODO (Kirill Gavrilov#9#) receive full server answer
-        // we assume buffer has inaf size
-        if(!stSocket.recv(buffer)) {
-            ST_DEBUG_LOG_AT("ERROR receiving...");
-            return false;
-        }
-        ST_DEBUG_LOG("Received:\n" + buffer.getData());
-
-        // TODO (Kirill Gavrilov#9#) process true XML parsing
-        static const char searchSubString[] = "<update>yes</update>";
-        size_t subStringLength = strlen(searchSubString);
-
-        for(size_t ch = 0; ch < buffer.getSize() - subStringLength; ch++) {
-            if(strncmp(&buffer[ch], searchSubString, subStringLength) == 0) {
-                ST_DEBUG_LOG("Updated version available!");
-                return true;
-            }
+        if(anAnswer.isContains(THE_TO_UPDATE)) {
+            ST_DEBUG_LOG("Updated version is available!");
+            return true;
         }
         return false;
     }
+
+        private:
+
+    static SV_THREAD_FUNCTION checkUpdatesThread(void* theThis) {
+        StCheckUpdates* aThis = (StCheckUpdates* )theThis;
+        if(StCheckUpdates::checkUpdates()) {
+            aThis->setNeedUpdate(true);
+        }
+        aThis->setInitialized();
+        return SV_THREAD_RETURN 0;
+    }
+
+        private:
+
+    StMutex            myMutex;
+    StHandle<StThread> myThread;
+    bool               myIsInitialized;
+    bool               myToUpdate;
 
 };
 
