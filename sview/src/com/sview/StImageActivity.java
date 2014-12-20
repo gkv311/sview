f74fa7624020a2d2f9a97470604121d0f7d7dfa0
Kirill Gavrilov <kirill@sview.ru>
2014-12-20 17:29:06 +0400

    Android Manifest - setup dedicated activities for Image Viewer and Movie Player

---
+++ sview/src/com/sview/StImageActivity.java
@@ -0,0 +1,23 @@
+/**
+ * This is source code for sView
+ *
+ * Copyright © Kirill Gavrilov, 2014
+ */
+package com.sview;
+
+import android.os.Bundle;
+
+/**
+ * Customize StActivity
+ */
+public class StImageActivity extends StActivity {
+
+    /**
+     * Create activity.
+     */
+    @Override
+    public void onCreate(Bundle theSavedInstanceState) {
+        super.onCreate(theSavedInstanceState);
+    }
+
+}
